ace716bc3177b41825d4b92ea131f6033f23bf86
rsbisne <roshansb99@gmail.com>
2023-06-19 12:49:32 +0530

    added user online/offline presence.

--- src/components/dashboard/DashboardToggle.js
+++ src/components/dashboard/DashboardToggle.js
@@ -1,22 +1,32 @@
 import React, { useCallback } from 'react';
-import { Alert, Button, Drawer, Icon } from 'rsuite';
-import { useMediaQuery, useModalState } from '../../misc/custom-hooks';
+import { Button, Icon, Drawer, Alert } from 'rsuite';
+import { useModalState, useMediaQuery } from '../../misc/custom-hooks';
 import Dashboard from '.';
-import { auth } from '../../misc/firebase';
+import { auth, database } from '../../misc/firebase';
+import { isOfflineForDatabase } from '../../context/profile.context';
 
 const DashboardToggle = () => {
   const { isOpen, close, open } = useModalState();
   const isMobile = useMediaQuery('(max-width: 992px)');
+
   const onSignOut = useCallback(() => {
-    auth.signOut();
-    Alert.info('Signed out', 4000);
-    close();
+    database
+      .ref(`/status/${auth.currentUser.uid}`)
+      .set(isOfflineForDatabase)
+      .then(() => {
+        auth.signOut();
+        Alert.info('Signed out', 4000);
+        close();
+      })
+      .catch(err => {
+        Alert.error(err.message, 4000);
+      });
   }, [close]);
+
   return (
     <>
       <Button block color="blue" onClick={open}>
-        <Icon icon="dashboard" />
-        Dashboard
+        <Icon icon="dashboard" /> Dashboard
       </Button>
       <Drawer full={isMobile} show={isOpen} onHide={close} placement="left">
         <Dashboard onSignOut={onSignOut} />
@@ -24,5 +34,4 @@ const DashboardToggle = () => {
     </>
   );
 };
-
 export default DashboardToggle;
--- src/context/profile.context.js
+++ src/context/profile.context.js
@@ -1,20 +1,38 @@
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
+import firebase from 'firebase';
 import { auth, database } from '../misc/firebase';
+
+export const isOfflineForDatabase = {
+  state: 'offline',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+  state: 'online',
+  last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
 const ProfileContext = createContext();
 
 export const ProfileProvider = ({ children }) => {
   const [profile, setProfile] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
     let userRef;
+    let userStatusRef;
+
     const authUnsub = auth.onAuthStateChanged(authObj => {
       if (authObj) {
+        userStatusRef = database.ref(`/status/${authObj.uid}`);
         userRef = database.ref(`/profiles/${authObj.uid}`);
+
         userRef.on('value', snap => {
-          const { name, createAt, avatar } = snap.val();
+          const { name, createdAt, avatar } = snap.val();
+
           const data = {
             name,
-            createAt,
+            createdAt,
             avatar,
             uid: authObj.uid,
             email: authObj.email,
@@ -22,18 +40,45 @@ export const ProfileProvider = ({ children }) => {
           setProfile(data);
           setIsLoading(false);
         });
+
+        database.ref('.info/connected').on('value', snapshot => {
+          if (!!snapshot.val() === false) {
+            return;
+          }
+
+          userStatusRef
+            .onDisconnect()
+            .set(isOfflineForDatabase)
+            .then(() => {
+              userStatusRef.set(isOnlineForDatabase);
+            });
+        });
       } else {
         if (userRef) {
           userRef.off();
         }
+
+        if (userStatusRef) {
+          userStatusRef.off();
+        }
+
+        database.ref('.info/connected').off();
+
         setProfile(null);
         setIsLoading(false);
       }
     });
     return () => {
       authUnsub();
+
+      database.ref('.info/connected').off();
+
       if (userRef) {
         userRef.off();
+      }
+
+      if (userStatusRef) {
+        userStatusRef.off();
       }
     };
   }, []);
